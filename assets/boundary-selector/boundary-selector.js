761cf6b3f8730c26f63c5edad019f2de4b58210f
unknown <brychan.thomas@btinternet.com>
2020-06-21 16:07:54 +0100

    Made regions' vertices draggable

--- assets/boundary-selector/boundary-selector.js
+++ assets/boundary-selector/boundary-selector.js
@@ -16,10 +16,23 @@ class Region {
     beginShape();
     for (let i=0; i<this.vertices.length; i++) {
       vertex(this.vertices[i].x, this.vertices[i].y);
-      circle(this.vertices[i].x, this.vertices[i].y, 10);
+      circle(this.vertices[i].x, this.vertices[i].y, 15);
     }
     vertex(this.vertices[0].x, this.vertices[0].y)
     endShape();
+  }
+
+  //checks if vertex is close to mouse and moves it if it is, returning true
+  //if no vertex close to mouse, returns false
+  drag(x, y) {
+    for (let i=0; i<this.vertices.length; i++) {
+      if (dist(this.vertices[i].x, this.vertices[i].y, x, y) < 30) {
+        this.vertices[i].x = x;
+        this.vertices[i].y = y;
+        return true;
+      }
+    }
+    return false;
   }
 }
 
@@ -41,12 +54,27 @@ function draw() {
   shapes.forEach(function(region) {
     region.display();
   });
+  if (mouseIsPressed) {
+    drag();
+  }
 }
 
 function keyPressed() {
+  console.log(key);
   //if they pressed a number key other than 0, 1 or 2
   if (['3','4','5','6','7','8','9'].includes(key)) {
     //add a new region with the number of sides
     shapes.push(new Region(mouseX, mouseY, Number(key)));
+  }
+}
+
+//called when mouse is held down to move shape vertices
+function drag() {
+  var foundVertex = false;
+  var i = 0;
+  while (!foundVertex && i < shapes.length) {
+    foundVertex = shapes[i].drag(mouseX, mouseY);
+    i++;
+    console.log(i);
   }
 }
